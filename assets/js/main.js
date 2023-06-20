380e76908676b4d0592afd389db3614ebcfb628f
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-06-20 14:28:31 +0100

    Added license and added template based on

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-/* main.js v1.0 | @ajlkn | MIT licensed */
+/*
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
 (function($) {
 
