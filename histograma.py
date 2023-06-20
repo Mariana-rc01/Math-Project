04e2c950a2b16ec147a404dfb7caf61da86079f0
AndreCFDias <100570083+AndreCFDias@users.noreply.github.com>
2023-06-20 17:32:27 +0100

    Update histograma.py

--- histograma.py
+++ histograma.py
@@ -4,13 +4,12 @@ import numpy as np
 
 st.header("Minha dashboard")
 
-mu = st.sidebar.number_input('Média:')
+mu = st.sidebar.number_input('Média:', step=0.1)
 
-sd = st.sidebar.number_input('SD:')
+sd = st.sidebar.number_input('SD:', min_value=0.0, value=2.0, step=0.1)
 
 # opcao = st.sidebar.selectbox("Distribuição", ("Normal", "Exponencial"))
 s = np.random.normal(mu, sd, 1000)
-fig, ax = plt.subplots()
-ax.hist(s, bins=20)
+plt.hist(s, bins=20)
 
-st.pyplot(fig)
+st.pyplot(plt)
