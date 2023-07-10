ac1d2533be433a0e363f3bce65ed8a74343f8d7e
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-07-10 13:07:49 +0100

    Second draft

--- analiseP2.py
+++ analiseP2.py
@@ -27,6 +27,12 @@ ax = fig.add_subplot(111, projection='3d')
 # Desenho do gráfico de f
 ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5)
 
+# Interseção do gráfico de f com o plano gerado por v e e3
+t = np.linspace(-1.5, 1.5, 100)
+C = v[0] * (A[0] + t * v[0]) + v[1] * (A[1] + t * v[1]) + f(A[0] + t * v[0], A[1] + t * v[1])
+curve_c = ax.plot(A[0] + t * v[0], A[1] + t * v[1], C, color='orange', label='Curva C')
+
+
 # Ponto A
 point_A = ax.scatter(A[0], A[1], A[2], color='blue', s=50, label='Ponto A')
 
@@ -40,6 +46,11 @@ ax.add_line(tangent_r)
 # Vetor v
 vetor_v = ax.quiver(A[0], A[1], A[2], v[0], v[1], v[2], color='red', label='Vetor v')
 
+# Ponto correspondente no gráfico de f -> Não está a aparecer as retas até ao final dos eixos x e y
+ax.plot([A[0], A[0]], [A[1], A[1]], [A[2], f(A[0], A[1])], linestyle='dashed', color='grey')
+ax.plot([A[0], 0], [A[1], A[1]], [0, 0], linestyle='dashed', color='grey')
+ax.plot([A[0], A[0]], [A[1], 0], [0, 0], linestyle='dashed', color='grey')
+
 # Configuração dos eixos e do título
 ax.set_xlabel('x')
 ax.set_ylabel('y')
@@ -47,7 +58,7 @@ ax.set_zlabel('z')
 ax.set_title('Gráfico de f com reta tangente r no ponto A')
 
 # Adicionando a legenda
-ax.legend([point_A, tangent_r, vetor_v], ['Ponto A', 'Reta tangente r', 'Vetor v'])
+ax.legend([point_A, tangent_r, vetor_v, curve_c[0]], ['Ponto A', 'Reta tangente r', 'Vetor v', 'Curva C'])
 
 # Mostrar o gráfico
 plt.show()
