ad0e12ea02b844c4b97b17acc8206851aaf1ff0a
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-07-11 12:50:30 +0100

    Changed all the comments to english

--- analiseP1.py
+++ analiseP1.py
@@ -2,25 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Função f(x, y)
+# Definition of the function f
 def f(x, y):
     return x**2 + y**2
 
-# Intervalo de valores para x e y
+# Range of values ​​for x and y
 x = np.linspace(-5, 5, 100)
 y = np.linspace(-5, 5, 100)
 X, Y = np.meshgrid(x, y)
 
-# Valores de z (f(x, y)) para os valores de x e y
+# Z values ​​(f(x, y)) for x and y values
 Z = f(X, Y)
 
-# Valores de c para as curvas de nível
+# c values ​​for contour lines
 c_values = np.linspace(np.min(Z), np.max(Z), 10)
 
-# Plot dos gráficos de f(x, y) e curvas de nível
+# Plot of graphs of f(x, y) and level curves
 fig = plt.figure(figsize=(12, 6))
 
-# Gráfico de f(x, y)
+# Graph of f(x, y)
 ax1 = fig.add_subplot(121, projection='3d')
 ax1.plot_surface(X, Y, Z, cmap='viridis', alpha=0.5)
 ax1.set_xlabel('X')
@@ -28,7 +28,7 @@ ax1.set_ylabel('Y')
 ax1.set_zlabel('Z')
 ax1.set_title('Gráfico de f(x, y)')
 
-# Curvas de nível em planos retangulares
+# Contours in rectangular planes
 ax2 = fig.add_subplot(122)
 ax2.contour(X, Y, Z, levels=30, cmap='viridis')
 for c in c_values:
--- analiseP3.py
+++ analiseP3.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Função f(x, y)
+# Definition of the function f
 def f(x, y):
     return x**2 + y**2
 
-# Ponto A
+# Point A
 A = (1, 2)
 
-# Intervalo e passo para criação do grid
+# Interval and step for grid creation
 x_range = np.linspace(-5, 5, 100)
 y_range = np.linspace(-5, 5, 100)
 
-# Criação do grid
+# Creating the grid
 X, Y = np.meshgrid(x_range, y_range)
 
-# Cálculo dos valores de f(x, y) para cada ponto do grid
+# Calculation of f(x, y) values ​​for each grid point
 Z = f(X, Y)
 
-# Desenhar o gráfico de f(x, y)
+# Draw the graph of f(x, y)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, cmap='viridis')
@@ -27,16 +27,16 @@ ax.set_ylabel('Y')
 ax.set_zlabel('f(x, y)')
 plt.title('Gráfico de f(x, y)')
 
-# Plotar ponto A e gradiente em A
-grad_f_x = 2 * A[0]  # Derivada parcial de f em relação a x
-grad_f_y = 2 * A[1]  # Derivada parcial de f em relação a y
+# Plot point A and gradient on A
+grad_f_x = 2 * A[0]  # Partial derivative of f with respect to x
+grad_f_y = 2 * A[1]  # Partial derivative of f with respect to y
 ax.scatter(A[0], A[1], f(A[0], A[1]), color='red', label='A')
 ax.quiver(A[0], A[1], f(A[0], A[1]), grad_f_x, grad_f_y, 0.1, color='blue', label='Gradiente em A')
 ax.legend()
 
-# Fazer mover um vetor u (aplicado em A e de norma 1)
-u = np.array([1.0, 0.0])  # Vetor u, altere os valores conforme necessário
-u /= np.linalg.norm(u)  # Normaliza o vetor u
+# Move a vector u (applied to A and with norm 1)
+u = np.array([1.0, 0.0])  # Vector u
+u /= np.linalg.norm(u)  # Normalizes the vector u
 ax.quiver(A[0], A[1], f(A[0], A[1]), u[0], u[1], 0.1, color='green', label='Vetor u')
 ax.legend()
 
