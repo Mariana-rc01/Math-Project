5f436083d5ae2973e6f6374d08807972e5a7555d
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-08-24 21:56:50 +0100

    Progress on Problem 4 of Analysis

--- static/js/analiseP4.js
+++ static/js/analiseP4.js
@@ -8,6 +8,16 @@ function g(x, y, i, j, k, l, m, n) {
 	return i * x**2 + j * y**2 + k * x * y + l * x + m * y + n;
 }
 
+// Função para calcular a derivada parcial de f(x,y)
+function dx(x, y, a, _, c, d, _, _) {
+	return 2 * a * x + c * y + d;
+}
+
+// Função para calcular a derivada parcial de f(x,y)
+function dy(x, y, _, b, c, _, e, _) {
+	return 2 * b * y + c * x + e;
+}
+
 // Função para calcular os valores de z para as curvas de nível
 function contourLevels(x, y, a, b, c, d, e, h) {
 	var Z = [];
@@ -21,16 +31,14 @@ function contourLevels(x, y, a, b, c, d, e, h) {
 	return Z;
 }
 
-// Calcula os pontos de interseção entre as superfícies f(x, y) e g(x, y) = 0
-function calculateIntersectionPoints(Z_f, Z_g) {
+// Calcula os pontos de interseção entre a superfície g(x, y) e o plano z = 0
+function calculateIntersectionPoints(Z_g) {
 	var intersectionPoints = [];
-	var numRows = Math.min(Z_f.length, Z_g.length);
-	var numCols = Math.min(Z_f[0].length, Z_g[0].length);
 
-	for (var i = 0; i < numRows; i++) {
-			for (var j = 0; j < numCols; j++) {
-					if (Z_f[i][j] === Z_g[i][j]) {
-							intersectionPoints.push({ x: y[j], y: x[i], z: 0 }); // Adiciona pontos à linha de interseção no plano z = 0
+	for (var i = 0; i < Z_g.length; i++) {
+			for (var j = 0; j < Z_g[i].length; j++) {
+					if (Z_g[i][j] === 0) {
+							intersectionPoints.push({ x: y[j], y: x[i], z: 0 });
 					}
 			}
 	}
@@ -55,6 +63,16 @@ function updateGraph() {
 		var m = parseFloat(document.getElementById('m').value) || 0; // Valor padrão de 0 para 'm'
 		var n = parseFloat(document.getElementById('n').value) || 0; // Valor padrão de 0 para 'n'
 
+		// Obter as coordenadas do ponto P
+		var pointX = parseFloat(document.getElementById('point-x').value) || 0;
+		var pointY = parseFloat(document.getElementById('point-y').value) || 0;
+
+		// Calcular o valor de z para o ponto P
+		var pointZ = f(pointX, pointY, a, b, c, d, e, h);
+
+		var vectorf = [dx(pointX, pointY, a, b, c, d, e, h), dy(pointX, pointY, a, b, c, d, e, h)];
+		var vectorg = [dx(pointX, pointY, i, j, k, l, m, n), dy(pointX, pointY, i, j, k, l, m, n)];
+
 		// Intervalo de valores para x e y
 		var x = [],
 				y = [];
@@ -65,44 +83,100 @@ function updateGraph() {
 
 		// Criação dos dados do gráfico 3D
 		var Z_f = contourLevels(x, y, a, b, c, d, e, h);
-		var Z_g = contourLevels(x, y, i, j, k, l, m, n);
-		var intersectionPoints = calculateIntersectionPoints(Z_f, Z_g); // Calcula os pontos de interseção
+		var Z_g = contourLevels(x, y, i, j, k, l, m, n); // Calcula os valores de g(x, y)
+		var intersectionPoints = calculateIntersectionPoints(Z_g); // Calcula os pontos de interseção
 
-		// Criação dos dados da linha de interseção
-		var lineData = {
-				type: 'scatter3d', // Usar scatter3d para representar os pontos da interseção como uma linha
-				mode: 'lines',
-				x: intersectionPoints.map(point => point.x),
-				y: intersectionPoints.map(point => point.y),
-				z: intersectionPoints.map(point => point.z),
-				line: {
-						color: 'red', // Ajuste a cor da linha de interseção conforme desejado
-						width: 2, // Ajuste a largura da linha de interseção conforme desejado
-				},
-				name: 'Interseção',
+		// Criação do ponto A no gráfico
+		var pointfP = {
+			type: 'scatter3d',
+			mode: 'markers',
+			x: [pointY],
+			y: [pointX],
+			z: [pointZ],
+			marker: {
+				color: 'pink',
+				size: 6,
+			},
+			name:'f(P)',
+		};
+
+		// Criação do ponto A no gráfico
+		var pointP = {
+			type: 'scatter3d',
+			mode: 'markers',
+			x: [pointY],
+			y: [pointX],
+			z: [0],
+			marker: {
+				color: 'magenta',
+				size: 6,
+			},
+			name:'P',
+		};
+
+		// Criação do vetor para o ponto A1
+		var vectorTracef = {
+			type: 'scatter3d',
+			mode: 'lines+markers',
+			x: [pointY, pointY + vectorf[1]], // Coordenadas x do ponto A1 e ponto final do vetor
+			y: [pointX, pointX + vectorf[0]], // Coordenadas y do ponto A1 e ponto final do vetor
+			z: [0, 0], // Coordenadas z do plano z=0 e ponto final do vetor
+			marker: {
+				color: 'green', // Cor dos marcadores do vetor
+				size: 3,
+			},
+			line: {
+				color: 'green', // Cor da linha do vetor
+				width: 2,
+			},
+			name: '∇f(P)', // Nome da legenda do vetor
+		};
+
+		// Criação do vetor para o ponto A1
+		var vectorTraceg = {
+			type: 'scatter3d',
+			mode: 'lines+markers',
+			x: [pointY, pointY + vectorg[1]], // Coordenadas x do ponto A1 e ponto final do vetor
+			y: [pointX, pointX + vectorg[0]], // Coordenadas y do ponto A1 e ponto final do vetor
+			z: [0, 0], // Coordenadas z do plano z=0 e ponto final do vetor
+			marker: {
+				color: 'orange', // Cor dos marcadores do vetor
+				size: 3,
+			},
+			line: {
+				color: 'orange', // Cor da linha do vetor
+				width: 2,
+			},
+			name: '∇g(P)', // Nome da legenda do vetor
 		};
 
 		var data3d = [
-				lineData, // Adicionar a linha de interseção aos dados
-				{
-						type: 'surface',
-						x: y,
-						y: x,
-						z: Z_f,
-						colorscale: 'Viridis',
-						showscale: false,
-						name: 'f(x, y)',
-				},
-				{
-						type: 'surface',
-						x: y,
-						y: x,
-						z: Z_g,
-						colorscale: 'Greys', // Escolha uma escala de cores adequada para o plano g(x, y) = 0
-						showscale: false,
-						opacity: 0.5, // Ajuste a opacidade conforme desejado
-						name: 'g(x, y) = 0',
-				},
+			{
+					type: 'scatter3d',
+					mode: 'markers',
+					x: intersectionPoints.map(point => point.x),
+					y: intersectionPoints.map(point => point.y),
+					z: intersectionPoints.map(point => point.z),
+					marker: {
+							size: 4,
+							color: 'red',
+					},
+					name: 'Interseção',
+			},
+			{
+					type: 'surface',
+					x: y,
+					y: x,
+					z: Z_f,
+					colorscale: 'Viridis', // Escolha uma escala de cores adequada
+					showscale: false,
+					opacity: 0.5,
+					name: 'f(x, y)',
+			},
+			pointP,
+			pointfP,
+			vectorTracef,
+			vectorTraceg,
 		];
 
 		// Definição das opções de layout dos gráficos
