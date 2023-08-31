e8deb136ef4648b2df72079226ab54e7e29b11ca
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-08-31 11:48:09 +0100

    Added another draft to problem 5

--- static/js/analiseP5.js
+++ static/js/analiseP5.js
@@ -6,6 +6,8 @@ function f(x, y, a, b, c, d, e, g) {
 // Número inicial de paralelepípedos
 let numParallelepipeds = 10;
 
+let soma = 0;
+
 // Função para criar os dados para o Plotly
 function createData() {
 	var a = parseFloat(document.getElementById('a').value) || 0;
@@ -32,15 +34,51 @@ function createData() {
 			const x1 = x0 + stepX;
 			const y1 = y0 + stepY;
 
-			const z = f((x0 + x1) / 2, (y0 + y1) / 2, a, b, c, d, e, g);
+			const zTop = f(x0,y0, a, b, c, d, e, g);
+			const zBase = 0;
+
+			const volume = zTop * Math.abs(x1 - x0) * Math.abs(y1 - y0);
+			soma = soma + volume;
+
+			const vertices = [
+				[x0, y0, zTop], [x1, y0, zTop], [x1, y1, zTop], [x0, y1, zTop],
+				[x0, y0, zBase], [x1, y0, zBase], [x1, y1, zBase], [x0, y1, zBase]
+			];
+
+			const faces = [
+				[0, 1, 2, 3], // Top
+				[2, 3, 0, 1], // Top
+
+				[4, 5, 6, 7], // Bottom
+				[6, 7, 4, 5], // Bottom
+
+				[0, 1, 5, 4], // Side
+				[5, 4, 0, 1], // Side
+
+				[1, 2, 6, 5], // Side
+				[6, 5, 1, 2], // Side
+
+				[2, 3, 7, 6], // Side
+				[7, 6, 2, 3], // Side
+
+				[3, 0, 4, 7], // Side
+				[4, 7, 3, 0] // Side
+			];
+
+			const xVertices = vertices.map(vertex => vertex[0]);
+			const yVertices = vertices.map(vertex => vertex[1]);
+			const zVertices = vertices.map(vertex => vertex[2]);
 
 			data.push({
 				type: 'mesh3d',
-				x: [x0, x1, x1, x0, x0],
-				y: [y0, y0, y1, y1, y0],
-				z: [0, 0, z, z, 0],
+				x: xVertices,
+				y: yVertices,
+				z: zVertices,
 				opacity: 0.5,
 				color: 'blue',
+				i: faces.map(face => face[0]),
+				j: faces.map(face => face[1]),
+				k: faces.map(face => face[2]),
 			});
 		}
 	}
@@ -69,6 +107,11 @@ function updateGraph() {
 	};
 
 	Plotly.newPlot(plotDiv, data, layout);
+
+	// Atualiza o valor da soma
+	const somaValue = document.getElementById('soma-value');
+	somaValue.textContent = soma.toFixed(2);
+
 }
 
 function onSlideChange() {
