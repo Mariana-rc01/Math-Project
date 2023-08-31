8b74114aa8a9e675a5fed7f2c19acf6d976ef624
Mariana-rc01 <rc.mariana.01@gmail.com>
2023-08-31 12:37:34 +0100

    Problem 5 done

--- static/js/analiseP5.js
+++ static/js/analiseP5.js
@@ -4,28 +4,49 @@ function f(x, y, a, b, c, d, e, g) {
 }
 
 // Número inicial de paralelepípedos
-let numParallelepipeds = 10;
+let numParallelepipeds = 30;
 
 let soma = 0;
 
 // Função para criar os dados para o Plotly
 function createData() {
-	var a = parseFloat(document.getElementById('a').value) || 0;
-	var b = parseFloat(document.getElementById('b').value) || 0;
-	var c = parseFloat(document.getElementById('c').value) || 0;
-	var d = parseFloat(document.getElementById('d').value) || 0;
-	var e = parseFloat(document.getElementById('e').value) || 0;
-	var g = parseFloat(document.getElementById('g').value) || 0;
+	soma = 0;
+	const a = parseFloat(document.getElementById('a').value) || 0;
+	const b = parseFloat(document.getElementById('b').value) || 0;
+	const c = parseFloat(document.getElementById('c').value) || 0;
+	const d = parseFloat(document.getElementById('d').value) || 0;
+	const e = parseFloat(document.getElementById('e').value) || 0;
+	const g = parseFloat(document.getElementById('g').value) || 0;
 
-	var xi = parseFloat(document.getElementById('xi').value) || 0;
-	var xf = parseFloat(document.getElementById('xf').value) || 0;
-	var yi = parseFloat(document.getElementById('yi').value) || 0;
-	var yf = parseFloat(document.getElementById('yf').value) || 0;
+	const xi = parseFloat(document.getElementById('xi').value) || 0;
+	const xf = parseFloat(document.getElementById('xf').value) || 0;
+	const yi = parseFloat(document.getElementById('yi').value) || 0;
+	const yf = parseFloat(document.getElementById('yf').value) || 0;
 
 	const stepX = (xf - xi) / numParallelepipeds;
 	const stepY = (yf - yi) / numParallelepipeds;
 
-	let data = [];
+	const data = [];
+	const vertices = Array(8);
+	const faces = [
+		[0, 1, 2, 3], // Top
+		[2, 3, 0, 1], // Top
+
+		[4, 5, 6, 7], // Bottom
+		[6, 7, 4, 5], // Bottom
+
+		[0, 1, 5, 4], // Side
+		[5, 4, 0, 1], // Side
+
+		[1, 2, 6, 5], // Side
+		[6, 5, 1, 2], // Side
+
+		[2, 3, 7, 6], // Side
+		[7, 6, 2, 3], // Side
+
+		[3, 0, 4, 7], // Side
+		[4, 7, 3, 0] // Side
+	];
 
 	for (let i = 0; i < numParallelepipeds; i++) {
 		for (let j = 0; j < numParallelepipeds; j++) {
@@ -38,32 +59,16 @@ function createData() {
 			const zBase = 0;
 
 			const volume = zTop * Math.abs(x1 - x0) * Math.abs(y1 - y0);
-			soma = soma + volume;
+			soma += volume;
 
-			const vertices = [
-				[x0, y0, zTop], [x1, y0, zTop], [x1, y1, zTop], [x0, y1, zTop],
-				[x0, y0, zBase], [x1, y0, zBase], [x1, y1, zBase], [x0, y1, zBase]
-			];
-
-			const faces = [
-				[0, 1, 2, 3], // Top
-				[2, 3, 0, 1], // Top
-
-				[4, 5, 6, 7], // Bottom
-				[6, 7, 4, 5], // Bottom
-
-				[0, 1, 5, 4], // Side
-				[5, 4, 0, 1], // Side
-
-				[1, 2, 6, 5], // Side
-				[6, 5, 1, 2], // Side
-
-				[2, 3, 7, 6], // Side
-				[7, 6, 2, 3], // Side
-
-				[3, 0, 4, 7], // Side
-				[4, 7, 3, 0] // Side
-			];
+			vertices[0] = [x0, y0, zTop];
+			vertices[1] = [x1, y0, zTop];
+			vertices[2] = [x1, y1, zTop];
+			vertices[3] = [x0, y1, zTop];
+			vertices[4] = [x0, y0, zBase];
+			vertices[5] = [x1, y0, zBase];
+			vertices[6] = [x1, y1, zBase];
+			vertices[7] = [x0, y1, zBase];
 
 			const xVertices = vertices.map(vertex => vertex[0]);
 			const yVertices = vertices.map(vertex => vertex[1]);
@@ -114,9 +119,20 @@ function updateGraph() {
 
 }
 
+function onInputChange() {
+	const input = document.getElementById('level-value');
+	const slider = document.getElementById('level');
+	const value = parseFloat(input.value) || 0;
+	slider.value = value;
+	numParallelepipeds = value;
+	updateGraph();
+}
+
 function onSlideChange() {
-	numParallelepipeds = document.getElementById('level').value;
+	const slider = document.getElementById('level');
+	numParallelepipeds = parseFloat(slider.value) || 0;
 	document.getElementById('level-value').value = numParallelepipeds;
+	soma = 0;
 	updateGraph();
 }
 
